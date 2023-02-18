f8fe6e9f3457f119bbb3a85aeea86bc88fee2e2f
Aayush <aayush@Aayushs-MacBook-Pro.local>
2023-02-18 22:58:48 +0530

    Updates

---
+++ precache-manifest.3d68960a6a01b7223fb01765bbd8288d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b4d4a56d9791db3aad4acd506964247",
+    "url": "/index.html"
+  },
+  {
+    "revision": "689d703749154ed00657",
+    "url": "/static/css/main.d0205105.chunk.css"
+  },
+  {
+    "revision": "2a53f26e9fe1199e6a8d",
+    "url": "/static/js/2.3d4365c4.chunk.js"
+  },
+  {
+    "revision": "075ee0fe56affc8020645735366f8cbd",
+    "url": "/static/js/2.3d4365c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "689d703749154ed00657",
+    "url": "/static/js/main.f50251be.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.f50251be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfed812ea91b71ebf252",
+    "url": "/static/js/runtime-main.8de15931.js"
+  },
+  {
+    "revision": "d6873c34759f79b87d52f39dbd86b407",
+    "url": "/static/media/mail.d6873c34.png"
+  },
+  {
+    "revision": "84e2a17f36a7ac185aaac999251d7669",
+    "url": "/static/media/profile.84e2a17f.png"
+  }
+]);
